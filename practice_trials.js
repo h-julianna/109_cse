e2e0386c286dd5d0d1233c842c7067457ae15da8
Peter Czingraber <peter.czingraber@gmail.com>
2026-01-28 12:49:35 +0100

    hardcoded practice trials for all participants in new js file

---
+++ practice_trials.js
@@ -0,0 +1,294 @@
+const prime_probe_prac_trials = {
+  "trials_block1": [
+      {
+        "prime": "le\nle\nle",
+        "probe": "le",
+        "congruency": "congruent",
+        "correct_response": "n",
+        "name": "magenta_vertical_con_1",
+        "monetary": 0,
+        "color": "magenta"
+      },
+      {
+        "prime": "bal\nbal\nbal",
+        "probe": "jobb",
+        "congruency": "incongruent",
+        "correct_response": "l",
+        "name": "blue_horizontal_incon_1",
+        "color": "blue",
+        "monetary": 0
+      },
+      {
+        "prime": "le\nle\nle",
+        "probe": "fel",
+        "congruency": "incongruent",
+        "correct_response": "e",
+        "name": "yellow_vertical_incon_2",
+        "color": "yellow",
+        "monetary": 0
+      },
+      {
+        "prime": "jobb\njobb\njobb",
+        "probe": "jobb",
+        "congruency": "congruent",
+        "correct_response": "l",
+        "name": "blue_horizontal_con_2",
+        "color": "blue",
+        "monetary": 0
+      },
+      {
+        "prime": "fel\nfel\nfel",
+        "probe": "le",
+        "congruency": "incongruent",
+        "correct_response": "n",
+        "name": "red_vertical_incon_1",
+        "color": "red",
+        "monetary": 1
+      },
+      {
+        "prime": "bal\nbal\nbal",
+        "probe": "bal",
+        "congruency": "congruent",
+        "correct_response": "a",
+        "name": "yellow_horizontal_con_1",
+        "color": "yellow",
+        "monetary": 0
+      },
+      {
+        "prime": "fel\nfel\nfel",
+        "probe": "le",
+        "congruency": "incongruent",
+        "correct_response": "n",
+        "name": "magenta_vertical_incon_1",
+        "monetary": 0,
+        "color": "magenta"
+      },
+      {
+        "prime": "jobb\njobb\njobb",
+        "probe": "jobb",
+        "congruency": "congruent",
+        "correct_response": "l",
+        "name": "blue_horizontal_con_2",
+        "color": "blue",
+        "monetary": 0
+      },
+      {
+        "prime": "le\nle\nle",
+        "probe": "fel",
+        "congruency": "incongruent",
+        "correct_response": "e",
+        "name": "magenta_vertical_incon_2",
+        "monetary": 0,
+        "color": "magenta"
+      },
+      {
+        "prime": "bal\nbal\nbal",
+        "probe": "bal",
+        "congruency": "congruent",
+        "correct_response": "a",
+        "name": "green_horizontal_con_1",
+        "color": "green",
+        "monetary": 1
+      },
+      {
+        "prime": "fel\nfel\nfel",
+        "probe": "fel",
+        "congruency": "congruent",
+        "correct_response": "e",
+        "name": "yellow_vertical_con_2",
+        "color": "yellow",
+        "monetary": 0
+      },
+      {
+        "prime": "bal\nbal\nbal",
+        "probe": "jobb",
+        "congruency": "incongruent",
+        "correct_response": "l",
+        "name": "red_horizontal_incon_1",
+        "color": "red",
+        "monetary": 1
+      },
+      {
+        "prime": "fel\nfel\nfel",
+        "probe": "le",
+        "congruency": "incongruent",
+        "correct_response": "n",
+        "name": "yellow_vertical_incon_1",
+        "color": "yellow",
+        "monetary": 0
+      },
+      {
+        "prime": "jobb\njobb\njobb",
+        "probe": "jobb",
+        "congruency": "congruent",
+        "correct_response": "l",
+        "name": "blue_horizontal_con_2",
+        "color": "blue",
+        "monetary": 0
+      },
+       {
+        "prime": "fel\nfel\nfel",
+        "probe": "fel",
+        "congruency": "congruent",
+        "correct_response": "e",
+        "name": "red_vertical_con_2",
+        "color": "red",
+        "monetary": 1
+      },
+      {
+        "prime": "jobb\njobb\njobb",
+        "probe": "bal",
+        "congruency": "incongruent",
+        "correct_response": "a",
+        "name": "green_horizontal_incon_2",
+        "color": "green",
+        "monetary": 1
+      }
+],
+  "trials_block2": [
+      {
+        "prime": "jobb\njobb\njobb",
+        "probe": "bal",
+        "congruency": "incongruent",
+        "correct_response": "a",
+        "name": "yellow_horizontal_incon_2",
+        "color": "yellow",
+        "monetary": 0
+      },
+      {
+        "prime": "fel\nfel\nfel",
+        "probe": "fel",
+        "congruency": "congruent",
+        "correct_response": "e",
+        "name": "green_vertical_con_2",
+        "color": "green",
+        "monetary": 1
+      },
+      {
+        "prime": "bal\nbal\nbal",
+        "probe": "bal",
+        "congruency": "congruent",
+        "correct_response": "a",
+        "name": "magenta_horizontal_con_1",
+        "monetary": 0,
+        "color": "magenta"
+      },
+      {
+        "prime": "le\nle\nle",
+        "probe": "le",
+        "congruency": "congruent",
+        "correct_response": "n",
+        "name": "blue_vertical_con_1",
+        "color": "blue",
+        "monetary": 0
+      },
+       {
+        "prime": "bal\nbal\nbal",
+        "probe": "jobb",
+        "congruency": "incongruent",
+        "correct_response": "l",
+        "name": "red_horizontal_incon_1",
+        "color": "red",
+        "monetary": 1
+      },
+      {
+        "prime": "le\nle\nle",
+        "probe": "le",
+        "congruency": "congruent",
+        "correct_response": "n",
+        "name": "green_vertical_con_1",
+        "color": "green",
+        "monetary": 1
+      },
+       {
+        "prime": "jobb\njobb\njobb",
+        "probe": "bal",
+        "congruency": "incongruent",
+        "correct_response": "a",
+        "name": "magenta_horizontal_incon_2",
+        "monetary": 0,
+        "color": "magenta"
+      },
+      {
+        "prime": "le\nle\nle",
+        "probe": "fel",
+        "congruency": "incongruent",
+        "correct_response": "e",
+        "name": "red_vertical_incon_2",
+        "color": "red",
+        "monetary": 1
+      },
+      {
+        "prime": "jobb\njobb\njobb",
+        "probe": "jobb",
+        "congruency": "congruent",
+        "correct_response": "l",
+        "name": "yellow_horizontal_con_2",
+        "color": "yellow",
+        "monetary": 0
+      },
+      {
+        "prime": "fel\nfel\nfel",
+        "probe": "le",
+        "congruency": "incongruent",
+        "correct_response": "n",
+        "name": "blue_vertical_incon_1",
+        "color": "blue",
+        "monetary": 0
+      },
+       {
+        "prime": "jobb\njobb\njobb",
+        "probe": "bal",
+        "congruency": "incongruent",
+        "correct_response": "a",
+        "name": "yellow_horizontal_incon_2",
+        "color": "yellow",
+        "monetary": 0
+      },
+      {
+        "prime": "le\nle\nle",
+        "probe": "le",
+        "congruency": "congruent",
+        "correct_response": "n",
+        "name": "green_vertical_con_1",
+        "color": "green",
+        "monetary": 1
+      },
+       {
+        "prime": "bal\nbal\nbal",
+        "probe": "jobb",
+        "congruency": "incongruent",
+        "correct_response": "l",
+        "name": "magenta_horizontal_incon_1",
+        "monetary": 0,
+        "color": "magenta"
+      },
+      {
+        "prime": "fel\nfel\nfel",
+        "probe": "fel",
+        "congruency": "congruent",
+        "correct_response": "e",
+        "name": "yellow_vertical_con_2",
+        "color": "yellow",
+        "monetary": 0
+      },
+      {
+        "prime": "bal\nbal\nbal",
+        "probe": "bal",
+        "congruency": "congruent",
+        "correct_response": "a",
+        "name": "green_horizontal_con_1",
+        "color": "green",
+        "monetary": 1
+      },
+      {
+        "prime": "le\nle\nle",
+        "probe": "fel",
+        "congruency": "incongruent",
+        "correct_response": "e",
+        "name": "blue_vertical_incon_2",
+        "color": "blue",
+        "monetary": 0
+      }
+    ]
+}
